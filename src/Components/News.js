0cce8e3846bb4840e49971df38121e737c27a7a7
Subham <subhamoffi@gmail.com>
2024-09-01 01:11:56 +0530

    Created the card component for News

--- src/Components/News.js
+++ src/Components/News.js
@@ -1,20 +1,34 @@
-import React, { Component } from 'react'
-import NewsItem from './NewsItem'
+import React, { Component } from "react";
+import NewsItem from "./NewsItem";
 
 export class News extends Component {
   render() {
     return (
-      <div>
-        This is a news component
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />
-        <NewsItem />        
+      //   <div>
+      //     This is a news component
+      //     <NewsItem />
+      //     <NewsItem />
+      //     <NewsItem />
+      //     <NewsItem />
+      //     <NewsItem />
+      //     <NewsItem />
+      //   </div>
+
+      <div className="container my-4">
+        <div className="row g-2">
+          <div className="col d-flex justify-content-evenly">
+            <NewsItem title="example title" description="example description" />
+          </div>
+          <div className="col d-flex justify-content-evenly">
+            <NewsItem />
+          </div>
+          <div className="col d-flex justify-content-evenly">
+            <NewsItem />
+          </div>
+        </div>
       </div>
-    )
+    );
   }
 }
 
-export default News
+export default News;
--- src/Components/NewsItem.js
+++ src/Components/NewsItem.js
@@ -1,13 +1,27 @@
-import React, { Component } from 'react'
+import React, { Component } from "react";
 
 export class NewsItem extends Component {
   render() {
+    let { title, description } = this.props;
     return (
-      <div>
-        this is a news item
-      </div>
-    )
+      <>
+        <div class="card" style={{width: "18rem"}}>
+          <img
+            src="https://ichef.bbci.co.uk/news/1024/branded_sport/27d8/live/96a5ec10-5940-11ef-94ec-63bde61d9499.jpg"
+            class="card-img-top"
+            alt="..."
+          />
+          <div class="card-body">
+            <h5 class="card-title">{title}</h5>
+            <p class="card-text">
+                {description}
+            </p>
+            
+          </div>
+        </div>
+      </>
+    );
   }
 }
 
-export default NewsItem
+export default NewsItem;
